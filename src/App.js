122a4a723ad85d84b9101bc3c68aa66b05ce9695
Peltonan <peltonen.annette@gmail.com>
2022-03-29 16:08:46 +0300

    Removed stuff.
    Added map and functions to questions.
    Made separate .js for questions to be stored

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
 import { useState } from 'react';
 import './App.css';
 import FirstAidKit from './images/favpng_first-aid-kit.png'
+import { questions } from './questions';
 
 function App() {
-  /*  const [counter, setCounter] = useState(0)
-   const clicked = () => {
-     let newNum = counter
-     newNum++
-     setCounter(newNum)
-   } */
   return (
 
     <div className="App">
       <h1>Feelings First Aid</h1>
       <img src={FirstAidKit} className="firstAidKit" alt="First Aid Kit"></img>
-      {/*       <h2>{counter}</h2>
-      <button onClick={clicked}>Click me!</button>
-    </div> */}
       <div className="intro">If you're feeling happy, fine or any positive feelings.
         You most likely don't need feelings first aid.</div>
       <div className="intro">Go live your life and enjoy!</div>
       <div>
         <div className="intro">Now I will ask you yes/no questions to figure out where you need band-aids&hearts;</div>
       </div>
-      <h2>1. Did you not get enough sleep this night? Have you slept too little most days this week?</h2>
-      <div className="wrapper">
-        <section>
+      {questions.map((questionBox, index) => {
+        console.log(index)
+        return (
           <div>
-            <input type="radio" id="question-one" className="answer" name="select" value="1" checked />
-            <label for="question-one">
-              <h3>YES</h3>
-              <p>I'm tired asf/I suffer from insomnia</p>
-            </label>
+            <h2>{index + 1}. {questionBox.question}</h2>
+            <div className="wrapper">
+              <section>
+                <div>
+                  <input type="radio" id={`question-one-${index}`} className="answer" name={`select-${index}`} value="1" checked />
+                  <label htmlFor={`question-one-${index}`} >
+                    <h3>YES</h3>
+                    <p>{questionBox.yesValue}</p>
+                  </label>
+                </div>
+                <div>
+                  <input type="radio" id={`question-two-${index}`} className="answer" name={`select-${index}`} value="2" />
+                  <label htmlFor={`question-two-${index}`}>
+                    <h3>NO</h3>
+                    <p>{questionBox.noValue}</p>
+                  </label>
+                </div>
+              </section>
+            </div>
           </div>
-          <div>
-            <input type="radio" id="question-two" className="answer" name="select" value="2" />
-            <label for="question-two">
-              <h3>NO</h3>
-              <p>I slept like a baby</p>
-            </label>
-          </div>
-        </section>
-      </div>
+        )
+      })}
+
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,19 @@
+export const questions = [
+    {
+        question: "Did you not get enough sleep this night? Have you slept too little most days this week?",
+        yesValue: "I'm tired asf/I suffer from insomnia",
+        noValue: "I slept like a baby"
+    },
+    {
+        question: "Do you have a lot of stress right now? Something big happening in your life?",
+        yesValue: "Yes, shit is going down, I can't calm down",
+        noValue: "No, everything is fine, I have peace of mind"
+    },
+    {
+        question: "Are you hungry? Have you forgotten to eat a proper meal?",
+        yesValue: "Yes, I'm hangry! Gimme food!",
+        noValue: "No, I'm fine, thank you"
+    },
+
+
+]
