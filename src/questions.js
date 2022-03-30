e1fcb42907a53731a756da1b49d63e9ee0edf0c4
Peltonan <peltonen.annette@gmail.com>
2022-03-30 20:13:17 +0300

    Added and changed questions

--- src/questions.js
+++ src/questions.js
@@ -6,14 +6,78 @@ export const questions = [
     },
     {
         question: "Do you have a lot of stress right now? Something big happening in your life?",
-        yesValue: "Yes, shit is going down, I can't calm down",
-        noValue: "No, everything is fine, I have peace of mind"
+        yesValue: "Shit is going down, I can't calm down",
+        noValue: "Everything is fine, I have peace of mind"
     },
     {
         question: "Are you hungry? Have you forgotten to eat a proper meal?",
-        yesValue: "Yes, I'm hangry! Gimme food!",
-        noValue: "No, I'm fine, thank you"
+        yesValue: "I'm hangry! Gimme food!",
+        noValue: "I'm fine, thank you"
     },
-
+    {
+        question: "Have you been inactive and spent most of the day inside?",
+        yesValue: "I've been glued to the couch",
+        noValue: "I've been outside, seen the world a bit"
+    },
+    {
+        question: "Have you spent the day not talking to anyone you like? Had to deal with difficult people?",
+        yesValue: "So f*cking annoying!",
+        noValue: "I've had a good time socializing"
+    },
+    {
+        question: "Has someone judged you or treated you badly today? Has someone hurt your feelings?",
+        yesValue: "I'm upset. Humans suck!",
+        noValue: "Everyone has treated me well"
+    },
+    {
+        question: "Is a certain person or issue troubling you? You keep thinking about one thing obsessively?",
+        yesValue: "I can't let it go, I'm stuck in a loop",
+        noValue: "Nothing bothers me right now"
+    },
+    {
+        question: "Are you thinking about bad memories, having negative thoughts about something?",
+        yesValue: "I keep spiraling into an abyss of shit",
+        noValue: "I'm in control of my mind"
+    },
+    {
+        question: "Do you regret or feel like you could be doing something differently?",
+        yesValue: "I've made some awful choices",
+        noValue: "I'm happy with how things are in my life"
+    },
+    {
+        question: "Have you been busy all day with work/obligations, not having any time for yourself?",
+        yesValue: "I haven't had any time for myself",
+        noValue: "I've had some alone time and done fun stuff"
+    },
+    {
+        question: "Are you shaming yourself? Are you shaming your body, weight or your looks?",
+        yesValue: "I don't like how I look, I want to change",
+        noValue: "I'm pretty happy with myself, flaws and all"
+    },
+    {
+        question: "Are you unhappy with yourself as a person? Are you not proud of yourself?",
+        yesValue: "I feel like I'm a bad person, I suck",
+        noValue: "I think I'm good, trying my best to improve"
+    },
+    {
+        question: "Are you not treating yourself? Have you been denying yourself something?",
+        yesValue: "I have hard discipline, and rarely treat myself",
+        noValue: "I treat myself, but don't over-indulge"
+    },
+    {
+        question: "Are you in pain? Is one or multiple parts on your body hurting?",
+        yesValue: "I'm in pain, and it's affecting my mood",
+        noValue: "All is well, I'm fit as a fiddle"
+    },
+    {
+        question: "Are you feeling lazy, bored or unmotivated? Lying around not doing anything?",
+        yesValue: "I can't bring myself to do anything",
+        noValue: "I got plans/I'm doing something I enjoy"
+    },
+    {
+        question: "Do you dislike/hate your work? Do you only live for the weekends?",
+        yesValue: "I can't stand my job!",
+        noValue: "I'm content, I wouldn't want to change it"
+    },
 
 ]
