8b142e689764de5d283b34b73149ab22c2bd6275
peltonan <peltonen.annette@gmail.com>
2022-04-04 10:30:14 +0300

    Add missing commas

--- src/questions.js
+++ src/questions.js
@@ -2,82 +2,98 @@ export const questions = [
     {
         question: "Did you not get enough sleep this night? Have you slept too little most days this week?",
         yesValue: "I'm tired asf/I suffer from insomnia",
-        noValue: "I've slept like a baby"
+        noValue: "I've slept like a baby",
+        answer: "You need to get some more sleep. Try getting earlier to bed today or take a short nap. If you suffer from insomnia, I recommend seeing a medical professional who can help you"
     },
     {
         question: "Do you have a lot of stress right now? Something big happening in your life?",
         yesValue: "Shit is going down, I can't calm down",
-        noValue: "Everything is fine, I have peace of mind"
+        noValue: "Everything is fine, I have peace of mind",
+        answer: "Stress is a part of life unfortunately, but most things will pass. However, try to find time to relax and do something you enjoy. Meditate, play video games, watch a movie, do something to let your head rest"
     },
     {
         question: "Are you hungry? Have you forgotten to eat a proper meal?",
         yesValue: "I'm hangry! Gimme food!",
-        noValue: "I'm fine, thank you"
+        noValue: "I'm fine, thank you",
+        answer: "Go eat something that makes you feel good. Try to avoid junk food, because that can eventually make you feel bad afterwards"
     },
     {
         question: "Have you been inactive and spent most of the day inside?",
         yesValue: "I've been glued to the couch",
-        noValue: "I've been outside, seen the world a bit"
+        noValue: "I've been outside, seen the world a bit",
+        answer: "Go outside for a walk or something. Changing your scenery and taking a bit of fresh air is good for you. Sometimes just 30min outside can make a big difference"
     },
     {
         question: "Have you spent the day not talking to anyone you like? Had to deal with difficult people?",
         yesValue: "So f*cking annoying!",
-        noValue: "I've had a good time socializing"
+        noValue: "I've had a good time socializing",
+        answer: "Try to find someone to talk to. Someone you like, trust and feel comfortable being around. Another option, is taking some time for yourself, taking a break from people"
     },
     {
         question: "Has someone judged you or treated you badly today? Has someone hurt your feelings?",
         yesValue: "I'm upset. Humans suck!",
-        noValue: "Everyone has treated me well"
+        noValue: "Everyone has treated me well",
+        answer: "Try not to linger for too long in the situation. Don't waste your energy on being angry at someone. Process everything, maybe talk to someone about it and then let it go. However, consider if this is something you need to confront. For example, a friend who keeps hurting you with bad behavior. Sleep on it, and take action the next day, if you feel it's necessary"
     },
     {
         question: "Is a certain person or issue troubling you? You keep thinking about one thing obsessively?",
         yesValue: "I can't let it go, I'm stuck in a loop",
-        noValue: "Nothing bothers me right now"
+        noValue: "Nothing bothers me right now",
+        answer: " Process your feelings, write them down or talk about them out loud. Talk to someone you can trust or a mental health professional. Obsessing over something is never good, try to find a way to let things go, if you can't change or do something about it. If you feel there's something you can do, sleep on it, and take action the next day. However, be sure to assess the situation, is it worth putting your energy in it. Try to picture the outcome."
     },
     {
         question: "Are you thinking about bad memories, having negative thoughts about something?",
         yesValue: "I keep spiraling into an abyss of shit",
-        noValue: "I'm in control of my mind"
+        noValue: "I'm in control of my mind",
+        answer: "Process things, if you haven't already done that, but don't give your thoughts control. If you realize, you keep slipping back, try to keep your mind occupied with other things. Do something that you enjoy, even if you don't feel like it, so your mind doesn't wander. Talk to someone you trust or see a mental health professional. Don't beat yourself up over your thoughts, this is very common."
     },
     {
         question: "Do you regret or feel like you could be doing something differently?",
         yesValue: "I've made some awful choices",
-        noValue: "I'm happy with how things are in my life"
+        noValue: "I'm happy with how things are in my life",
+        answer: "Regretting something is a way to punish yourself, you can't change the past. Try to think about everything as a lesson, what did you learn from a decision that didn't turn out well? However, if you can make a change, sit down and write down what you can do to make a change for the better. Make a list, with small steps. What little thing can you do today? Every journey starts with one step, try to avoid taking too much at once, that will in most cases lead to discouraging feelings."
     },
     {
         question: "Have you been busy all day with work/obligations, not having any time for yourself?",
         yesValue: "I haven't had any time for myself",
-        noValue: "I've had some alone time and done fun stuff"
+        noValue: "I've had some alone time and done fun stuff",
+        answer: "Make time for yourself. Take a break. Do something you enjoy. I know it might not be easy, if you have a busy schedule. But you and your brain will thank you for a much needed break."
     },
     {
         question: "Are you shaming yourself? Are you shaming your body, weight or your looks?",
         yesValue: "I don't like how I look, I want to change",
-        noValue: "I'm pretty happy with myself, flaws and all"
+        noValue: "I'm pretty happy with myself, flaws and all",
+        answer: "It can be be tough to be happy with yourself. But there's only one you, and you're unique in your own way. We're born and carry some things with us, no matter if we like it or not. If your issue is fixable, for example, you want to lose weight, take small steps or find someone who can guide you. But remember to love yourself, everyone is flawed, no one is perfect."
     },
     {
         question: "Are you unhappy with yourself as a person? Are you not proud of yourself?",
         yesValue: "I feel like I'm a bad person, I suck",
-        noValue: "I think I'm good, trying my best to improve"
+        noValue: "I think I'm good, trying my best to improve",
+        answer: "If there's something you can do to change or improve yourself, figure out how to do it and take small steps. However, don't be too hard on yourself and take a moment to think about what you have achieved so far, no matter how small or big."
     },
     {
         question: "Are you not treating yourself? Have you been denying yourself something?",
         yesValue: "I have hard discipline, and rarely treat myself",
-        noValue: "I treat myself, but don't over-indulge"
+        noValue: "I treat myself, but don't over-indulge",
+        answer: "It's good to not over-indulge, but think about something you want or something that could improve your life. It doesn't always have to be something big, even a small thing like a new coffee mug can bring joy every morning. Remember to treat yourself once in awhile."
     },
     {
         question: "Are you in pain? Is one or multiple parts on your body hurting?",
         yesValue: "I'm in pain, and it's affecting my mood",
-        noValue: "All is well, I'm fit as a fiddle"
+        noValue: "All is well, I'm fit as a fiddle",
+        answer: " It's more difficult to be in a good mood if you're in pain. Depending on your injury, be sure to rest and take small steps in recovering. Visit a medical professional if the pain doesn't go away by itself. (If you suffer from chronic pain, I'm sorry. Remember to be kind to yourself, take time to process your limits, if you haven't done that already.) "
     },
     {
         question: "Are you feeling lazy, bored or unmotivated? Lying around not doing anything?",
         yesValue: "I can't bring myself to do anything",
-        noValue: "I got plans/I'm doing something I enjoy"
+        noValue: "I got plans/I'm doing something I enjoy",
+        answer: "Get up and start doing something you enjoy, chores or whatever. Even if you're not feeling motivated and it feels difficult to get up. After awhile you might get a flow, really get into what you're doing or having fun. Things rarely get better by doing nothing. Take action."
     },
     {
         question: "Do you dislike/hate your work? Do you only live for the weekends?",
         yesValue: "I can't stand my job!",
-        noValue: "I'm content, I wouldn't want to change it"
+        noValue: "I'm content, I wouldn't want to change it",
+        answer: "A new job or a career switch is in order. Take time figuring out what you want and need. Start saving up. Start looking for a new job. Take small steps to get somewhere. It can be scary, but would you rather take a risk or be miserable for the rest of your life? You only got one life, please don't waste it."
     },
 
 ]
